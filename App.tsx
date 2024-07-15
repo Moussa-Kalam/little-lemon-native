f28e658208e0e6a24f28517e44a033f5dc5ce15a
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-15 15:48:49 +0200

    feat: build a scrollable welcome screen

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ export default function App() {
         }}
       >
         <LittleLemonHeader />
+        <WelcomeScreen />
       </View>
       <View style={{ backgroundColor: '#495E57' }}>
         <LittleLemonFooter />
